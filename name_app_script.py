61e2af45bd9ec8ebf3e3efa5620100934e52deec
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-09-26 08:52:50 +0100

    set up flask framework in python
    url's and different pages

--- name_app_script.py
+++ name_app_script.py
@@ -1,5 +1,24 @@
 from flask import Flask, render_template, request, Response
 
-#insert content here
+#Home page
+@app.route("/about")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+def landing_page():
+        return render_template("landing_page.html") #runs the landing page
+
+#Input page
+@app.route("/input")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+def landing_page():
+        return render_template("user_input_page.html") #runs the landing page
+
+#Results
+@app.route("/results")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+def landing_page():
+        return render_template("user_results_page.html") #runs the landing page
+
+
+#About page
+@app.route("/about")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+def landing_page():
+        return render_template("about_page.html") #runs the landing page
 
 app.run(debug=True) #runs the app. the debug part - unlocks debugging feature.
