5c586a89da8e99e2d51f4c70628aa5fdb7f85138
Mio Chung <miochung7@gmail.com>
2020-09-26 16:47:40 +0100

    image-upload functionality

--- nomi_app_script.py
+++ nomi_app_script.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, redirect, request, Response
+import os
 
 app = Flask("nomi_app") #making an app
+
+# Specifies a path to save images and allows image types
+
+app.config["IMAGE_UPLOADS"] = "/Users/Mio/Desktop/name_app/static/images/uploads"
+
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPEG", "JPG", "GIF"]
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 
 
 #Home page
 @app.route("/about")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
@@ -8,8 +16,43 @@ def landing_page():
         return render_template("about_page.html") #runs the landing page
 
 #Input page
-@app.route("/input")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+@app.route("/input", methods=["GET", "POST"])    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+
+# Checks if extension is allowed
+def allowed_image(filename):
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1] #Splits from the right on the . and takes the first element 
+    
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:   
+        return False
+
 def landing_page():
+
+        if request.method == "POST":
+
+                if request.files:
+
+                        image = request.files["image"]
+
+                        if image.filename == "":
+                                print("Image must have a filename")
+                                return redirect(request.url)
+
+                        if not allowed_image(image.filename):
+                                print("That image extension is not allowed")
+                                return redirect(request.url)
+
+                        image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
+
+                        print('Image saved!')
+
+                        return redirect(request.url)
+
         return render_template("user_input_page.html") #runs the landing page
 
 #Results
