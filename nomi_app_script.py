b3c52b81a7f7605d9ecc09efc8fe5988730381db
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2020-09-29 19:03:06 +0100

    building jinja2 interface

--- nomi_app_script.py
+++ nomi_app_script.py
@@ -1,37 +1,34 @@
 from flask import Flask, render_template, request, Response
+from jinja2 import Template
 
 app = Flask("nomi_app") #making an app
 
-#Home page
-@app.route("/about")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
-def landing_page():
-        return render_template("about_page.html") #runs the landing page
-
 #Input page
-@app.route("/input")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
-def landing_page():
+@app.route("/")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+def input_page():
         return render_template("user_input_page.html") #runs the landing page
 
 #Results
-@app.route("/results", methods=['POST'])    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
-def landing_page():
+@app.route("/results", methods=['GET', 'POST'])    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
+def results_page(firstName):
         form_data = request.form
 
-        FirstName = form_data["FirstName"]
-        LastName = form_data["LastName"]
-        NameScriptImageFirstName = form_data["NameScriptImageFirstName"]
-        NameScriptImageLastName = form_data["NameScriptImageLastName"]
+        gender = form_data["selectGender"]
+        firstName = form_data["FirstName"]
+        lastName = form_data["LastName"]
+        FirstNameImage = form_data["FirstNameImage"]
+        LastNameImageme = form_data["LastNameImage"]
         FirstNamePronounciation = form_data["FirstNamePronounciation"]
         LastNamePronounciation = form_data["LastNamePronounciation"]        
-        FreeTextContentFirstName = form_data["FreeTextContentFirstName"]        
-        FreeTextContentLastName = form_data["FreeTextContentLastName"]  
+        FreeTextContentFirstName = form_data["FirstNameFreeTextContent"]        
+        FreeTextContentLastName = form_data["LastNameFreeTextContent"]  
 
-        return render_template("display_page.html") #runs the landing page
+        return render_template("display_page.html", firstName=firstName.results_page())
 
 
 #About page
 @app.route("/about")    #@ makes it a 'decorator'. line tells peple where to look inside flask framework. Decorators always followed by function.
-def landing_page():
+def about_page():
         return render_template("about_page.html") #runs the landing page
 
 app.run(debug=True) #runs the app. the debug part - unlocks debugging feature.
