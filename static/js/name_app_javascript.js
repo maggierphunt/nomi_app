c9c49369f68b23079d86a3800e4ce73369a18dac
Mio Chung <miochung7@gmail.com>
2020-10-01 08:59:55 +0100

    Added dark mode JS

--- static/js/name_app_javascript.js
+++ static/js/name_app_javascript.js
@@ -1,10 +1,13 @@
-const dropdown = document.querySelector('.dropdown');
+const dropdownContainer = document.querySelector('.dropdown');
+
 const zoomInOut = document.querySelector('.zoom-inout');
+
 const switchContainer = document.querySelector('.switch-container');
+
 const dropdownSocial = document.querySelector('.dropdown-social');
 
 function myFunction() {
-	dropdown.classList.toggle('show');
+	dropdownContainer.classList.toggle('show');
 
 	zoomInOut.style.display = 'none';
 
@@ -31,4 +34,39 @@ function clearShare() {
 
 // document.addEventListener('click', function(event) {
 
-console.log('hello');
+// Dark Mode Toggle
+
+function darkmodeToggle() {
+	const body = document.body;
+	body.classList.toggle('dark-mode');
+
+	const header = document.querySelector('.header-container');
+	header.classList.toggle('dark-mode2');
+
+	const footer = document.querySelector('.footer-container');
+	footer.classList.toggle('dark-mode2');
+}
+
+// Image Upload
+
+// Add the following code if you want the name of the file appear on select
+// $('.custom-file-input').on('change', function() {
+// 	var fileName = $(this).val().split('\\').pop();
+// 	$(this).siblings('.custom-file-label').addClass('selected').html(fileName);
+// });
+
+const customFileInput = document.querySelector('.custom-file-input');
+const customFileLabel = document.querySelector('.custom-file-label');
+
+const customRecording = document.querySelector('.recording');
+const customRecordingLabel = document.querySelector('.recording-label');
+
+customFileInput.addEventListener('change', function() {
+	const fileName = customFileInput.value.split(/[\\\/]/).pop();
+	customFileLabel.innerHTML = fileName;
+});
+
+customRecording.addEventListener('change', function() {
+	const recordingFileName = customRecording.value.split(/[\\\/]/).pop();
+	customRecordingLabel.innerHTML = recordingFileName;
+});
